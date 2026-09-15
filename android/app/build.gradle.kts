30465d890364957afb92f592b816748061253a63
Mochamad Yusuf <mochyusuf100@gmail.com>
2026-09-15 18:24:09 +0700

    fix icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kamus_arab"
+    namespace = "com.mochyusuf.kamus_arab"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kamus_arab"
+        applicationId = "com.mochyusuf.kamus_arab"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/kamus_arab/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kamus_arab/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kamus_arab
+package com.mochyusuf.kamus_arab
 
 import io.flutter.embedding.android.FlutterActivity
 
